3e4eb38fe831a7e2de68081cc69db0214b40e85d
zerozayu <304307556@qq.com>
2020-04-12 20:57:22 +0800

    2020.4.12

--- app/src/main/java/com/example/irsis/ChatActivity.java
+++ app/src/main/java/com/example/irsis/ChatActivity.java
@@ -1,18 +1,27 @@
 package com.example.irsis;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.core.content.FileProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.Manifest;
 import android.annotation.SuppressLint;
+import android.annotation.TargetApi;
+import android.content.ContentUris;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.provider.DocumentsContract;
 import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
@@ -20,6 +29,7 @@ import android.widget.EditText;
 import android.widget.GridLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import com.example.irsis.adapter.MsgAdapter;
 import com.example.irsis.myclass.Msg;
@@ -36,12 +46,14 @@ public class ChatActivity extends BaseActivity implements View.OnClickListener {
     private ImageView picture;
     private Uri imageUri;
 
+    public static final int CHOOSE_PHOTO = 2;
+
 
     private List<Msg> msgList = new ArrayList<>();
     private EditText inputText;
-    protected ImageView button_listFunction;
     protected ImageView button_sendMessage;
     protected ImageView button_takePhoto;
+    protected ImageView button_choosePicture;
 
     private LinearLayout listFunction_layout;
     private RecyclerView msgRecyclerView;
@@ -66,17 +78,19 @@ public class ChatActivity extends BaseActivity implements View.OnClickListener {
         inputText = findViewById(R.id.input_text);
         inputText.setOnClickListener(this);
 
-        button_listFunction = findViewById(R.id.button_listFunction);
-        button_listFunction.setOnClickListener(this);
 
+        //发送
         button_sendMessage = findViewById(R.id.button_sendMessage);
         button_sendMessage.setOnClickListener(this);
-
+        //拍照
         button_takePhoto = findViewById(R.id.button_takePhoto);
         button_takePhoto.setOnClickListener(this);
-
+        //选取照片
+        button_choosePicture = findViewById(R.id.button_choosePicture);
+        button_choosePicture.setOnClickListener(this);
+        //图片
         picture = findViewById(R.id.picture);
-
+        //拍照、选取照片等选项
         listFunction_layout = findViewById(R.id.listFunction_layout);
         msgRecyclerView = findViewById(R.id.msg_recycler_view);
 
@@ -104,17 +118,6 @@ public class ChatActivity extends BaseActivity implements View.OnClickListener {
                     inputText.setText("");
                 }
                 break;
-            case R.id.button_listFunction:
-                if (listFunction_layout.getVisibility() == View.GONE) {
-                    listFunction_layout.setVisibility(View.VISIBLE);
-                } else {
-                    listFunction_layout.setVisibility(View.GONE);
-                }
-                break;
-            case R.id.input_text:
-                listFunction_layout.setVisibility(View.GONE);
-                break;
-
             case R.id.button_takePhoto:
                 //创建File对象，用于存储拍照后的图片
                 File outputImage = new File(getExternalCacheDir(), "output_image.jpg");
@@ -139,9 +142,17 @@ public class ChatActivity extends BaseActivity implements View.OnClickListener {
                 Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
                 intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
                 startActivityForResult(intent, TAKE_PHOTO);
-
                 break;
 
+            case R.id.button_choosePicture:
+                if (ContextCompat.checkSelfPermission(ChatActivity.this,
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(ChatActivity.this, new String[]
+                            {Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+                } else {
+                    openAlbum();
+                }
+                break;
             default:
                 break;
         }
@@ -162,8 +173,97 @@ public class ChatActivity extends BaseActivity implements View.OnClickListener {
                     picture.setImageBitmap(bitmap);
                 }
                 break;
+            case CHOOSE_PHOTO:
+                if(resultCode==RESULT_OK){
+                    //判断手机系统版本号
+                    if (Build.VERSION.SDK_INT>=19){
+                        //4.4及以上使用此方法处理图片
+                        handleImageOnKitKat(data);
+                    }else {
+                        //4.4以下系统使用这个方法处理图片
+                        handleImageBoforeKitKat(data);
+                    }
+                }
+                break;
             default:
                 break;
+        }
+    }
+
+    //自定义方法打开相册
+    private void openAlbum() {
+        Intent intent = new Intent("android.intent.action.GET_CONTENT");
+        intent.setType("image/*");
+        startActivityForResult(intent, CHOOSE_PHOTO);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case 1:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    openAlbum();
+                } else {
+                    Toast.makeText(this, "没有相册的权限！", Toast.LENGTH_SHORT).show();
+                }
+                break;
+            default:
+        }
+    }
+
+    @TargetApi(19)
+    private void handleImageOnKitKat(Intent data){
+        String imagePath=null;
+        Uri uri=data.getData();
+
+        if(DocumentsContract.isDocumentUri(this,uri)){
+            //如果是document类型的Uri，则通过document id处理
+            String docId=DocumentsContract.getDocumentId(uri);
+            if ("com.android.provides.media.documents".equals(uri.getAuthority())){
+                String id =docId.split(":")[1];//解析出数字格式的id
+                String selection  =MediaStore.Images.Media._ID+"="+id;
+                imagePath=getImagePath(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,selection);
+            }else if("com.android.provides.downloads.documents".equals(uri.getAuthority())){
+                Uri contentUri = ContentUris.withAppendedId(Uri.parse("content://downloads/public_downloads"),
+                        Long.valueOf(docId));
+                imagePath=getImagePath(contentUri,null);
+            }
+        }else if ("content".equalsIgnoreCase(uri.getScheme())){
+            //如果是content类型的Uri，则使用普通方式处理
+            imagePath=getImagePath(uri,null);
+        }else if ("file".equalsIgnoreCase(uri.getScheme())){
+            //如果是file类型的Uri，直接获取图片路径即可
+            imagePath=uri.getPath();
+        }
+        //根据图片路径显示图片
+        displayImage(imagePath);
+    }
+
+    private void handleImageBoforeKitKat(Intent data){
+        Uri uri=data.getData();
+        String imagePath =getImagePath(uri,null);
+        displayImage(imagePath);
+    }
+
+    private String getImagePath(Uri uri, String selection){
+        String path=null;
+        //通过Uri和selection来获取真实的图片路径
+        Cursor cursor =getContentResolver().query(uri,null,selection,null,null);
+        if(cursor!=null){
+            if(cursor.moveToFirst()){
+                path=cursor.getString(cursor.getColumnIndex(MediaStore.Images.Media.DATA));
+            }
+            cursor.close();
+        }
+        return path;
+    }
+
+    private void displayImage(String imagePath){
+        if(imagePath!=null){
+            Bitmap bitmap=BitmapFactory.decodeFile(imagePath);
+            picture.setImageBitmap(bitmap);
+        }else {
+            Toast.makeText(this,"获取图片失败！",Toast.LENGTH_SHORT).show();
         }
     }
 }
--- app/src/main/java/com/example/irsis/SubmitProblemActivity.java
+++ app/src/main/java/com/example/irsis/SubmitProblemActivity.java
@@ -1,15 +1,25 @@
 package com.example.irsis;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
 
+import android.annotation.SuppressLint;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.Image;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.example.irsis.Fragment.ProblemNameFragment;
@@ -17,10 +27,17 @@ import com.example.irsis.myclass.Problem;
 
 import org.litepal.LitePal;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 public class SubmitProblemActivity extends BaseActivity implements View.OnClickListener {
+    public static final int TAKE_PHOTO = 2;
+    private Uri imageUri;
 
     private EditText edit_problemName;
     private EditText edit_problemContent;
+    private ImageView picture_problem;
 
 
     @Override
@@ -30,13 +47,19 @@ public class SubmitProblemActivity extends BaseActivity implements View.OnClickL
         //数据库
         LitePal.getDatabase();
 
-        //声明组件
+        //声明组件:问题名字、问题内容、图片显示(默认invisible)、拍照按钮、提交按钮
         edit_problemName = findViewById(R.id.editText_problemName);
         edit_problemContent = findViewById(R.id.editText_problemContent);
 
+        picture_problem = findViewById(R.id.picture_problem);
+
+
+        ImageView button_takePhoto = findViewById(R.id.button_takePhoto_problem);
+        button_takePhoto.setOnClickListener(this);
 
         Button button_submitProblem = findViewById(R.id.button_submitProblem);
         button_submitProblem.setOnClickListener(this);
+
     }
 
     @Override
@@ -46,7 +69,7 @@ public class SubmitProblemActivity extends BaseActivity implements View.OnClickL
             case R.id.button_submitProblem:
                 //数据库存储操作
                 Problem problem = new Problem();
-                 //problem.setpId(1);
+                //problem.setpId(1);
                 problem.setName(edit_problemName.getText().toString());
                 problem.setContent(edit_problemContent.getText().toString());
                 problem.setImageId(R.drawable.problem128);
@@ -64,6 +87,52 @@ public class SubmitProblemActivity extends BaseActivity implements View.OnClickL
                     }
                 });
                 builder.show();
+                break;
+
+            case R.id.button_takePhoto_problem:
+                File outputProblemImage = new File(getExternalCacheDir(), "output_problemImage");
+                if (outputProblemImage.exists()) {
+                    outputProblemImage.delete();
+                }
+                try {
+                    outputProblemImage.createNewFile();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                if (Build.VERSION.SDK_INT >= 24) {
+                    imageUri = FileProvider.getUriForFile(SubmitProblemActivity.this,
+                            "com.example.irsis.fileprovider", outputProblemImage);
+                } else {
+                    imageUri = Uri.fromFile(outputProblemImage);
+                }
+                //启动相机程序
+                Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+                startActivityForResult(intent, TAKE_PHOTO);
+
+                break;
+
+
+            default:
+                break;
+        }
+    }
+
+    @SuppressLint("MissingSuperCall")
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        switch (requestCode) {
+            case TAKE_PHOTO:
+                if (resultCode == RESULT_OK) {
+                    Bitmap bitmap = null;
+                    try {
+                        bitmap = BitmapFactory.decodeStream(getContentResolver().openInputStream(imageUri));
+                    } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                    }
+                    picture_problem.setImageBitmap(bitmap);
+
+                }
                 break;
             default:
                 break;
